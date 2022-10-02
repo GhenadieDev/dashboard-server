cfbd31b869c3b66da858307a3654be5c03e7346a
Ghenadie Hilotii <hilotiighenadie0@gmail.com>
2022-10-02 10:41:41 +0000

    u

--- app/config/config.server.js
+++ app/config/config.server.js
@@ -5,23 +5,14 @@ import postRoutes from "../routes/postRoutes.js";
 import userRoutes from "../routes/userRoutes.js";
 const server = express();
 
-/*server.use(
+server.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    preflightContinue: true,
   })
-);*/
-
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Allow-Control-Allow-Credentials", "true");
-  next();
-});
+);
 
 server.use(express.json());
 server.use(cookieParser());
