e6da1520beef16fa65403214b1d4fbbf496b32cc
Ghenadie Hilotii <hilotiighenadie0@gmail.com>
2022-10-06 12:17:46 +0000

    changed auth

--- app/routes/authRoute.js
+++ app/routes/authRoute.js
@@ -4,7 +4,7 @@ import checkToken from "../middlewares/checkToken.js";
 const route = express.Router();
 
 route.get("/auth", checkToken, (req, res) => {
-  return res.status(200).json({ msg: "ok" });
+  return res.status(200).json({ message: "ok" });
 });
 
 export default route;
