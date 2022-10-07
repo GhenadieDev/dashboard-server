024e8bc8a945f37e074ce96b6a9a7d8e19913b18
Ghenadie Hilotii <hilotiighenadie0@gmail.com>
2022-10-07 19:09:48 +0000

    b

--- app/config/config.server.js
+++ app/config/config.server.js
@@ -6,12 +6,30 @@ import userRoutes from "../routes/userRoutes.js";
 import authRoute from "../routes/authRoute.js";
 const server = express();
 
-server.use(
-  cors({
-    credentials: true,
-    origin: ["http://localhost:3000"],
-  })
-);
+const whitelist = ["https://react-dashboard-rzy3.vercel.app"]; //white list consumers
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "device-remember-token",
+    "Access-Control-Allow-Origin",
+    "Origin",
+    "Accept",
+  ],
+};
+
+server.use(cors(corsOptions));
 
 server.use(express.json());
 server.use(cookieParser());
