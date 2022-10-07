286314874376fca9752a0de095c48f3a831b8b85
Ghenadie Hilotii <hilotiighenadie0@gmail.com>
2022-10-07 19:46:54 +0000

    q

--- app/config/config.server.js
+++ app/config/config.server.js
@@ -15,7 +15,7 @@ const corsOptions = {
       callback(null, false);
     }
   },
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  methods: ["GET", "PATCH", "POST", "DELETE", "OPTIONS"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
   allowedHeaders: [
